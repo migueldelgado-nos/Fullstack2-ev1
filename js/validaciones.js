f7650d8689299e18bb4a876b579c4796795b4da2
migueldeg <migu.delgado@duocuc.cl>
2026-09-16 01:33:09 -0300

    Update validaciones.js
    Final

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,33 +1,10 @@
 // VALIDACIÓN REGISTRO DE USUARIO
 
 const formRegistro = document.getElementById("formRegistro");
-const formularioDatos = document.getElementById("formularioDatos");
-const formularioDatosForm = document.getElementById("formulario-datos");
-const formularioAlimentacion = document.getElementById("formularioAlimentacion");
-const formularioAlimentacionForm = document.getElementById("formulario-alimentacion");
-const campoNombre =document.getElementById("nombre");
-const campoEdad = document.getElementById("edad");
-const campoSexo = document.getElementById("sexo");
-const campoPeso = document.getElementById("peso");
-const campoTallaCintura = document.getElementById("cintura");
-const campoAltura = document.getElementById("altura");
-const campoGrasaCorporal = document.getElementById("grasa");
-const mensajeResultado = document.getElementById("mensaje-resultado");
-
-const campoDesayuno=document.getElementById("desayuno");
-const campoColacion=document.getElementById("colacion1");
-const campoAlmuerzo=document.getElementById("almuerzo");
-const campoColacionTarde=document.getElementById("colacion2");
-const campoCena=document.getElementById("cena");
-const campoObservaciones=document.getElementById("observaciones");
 
 if (formRegistro) {
 
     formRegistro.addEventListener("submit", function (event) {
-
-    } 
-    if (tipoFormulario.value === "alimentacion"){
-
 
         event.preventDefault();
 
@@ -137,58 +114,3 @@ botonesCarrito.forEach(function (boton) {
     });
 
 });
-
-formularioDatosForm.addEventListener("submit",function(evento){
-    evento.preventDefault();
-    const nombre = campoNombre.value.trim();
-    const edad = Number(campoEdad.value);
-    const sexo = campoSexo.value;
-    const peso = Number(campoEdad.value);
-    const tallaCintura = Number(campoTallaCintura.value);
-    const altura = Number(campoAltura.value);
-    const grasaCorporal = Number(campoGrasaCorporal.value);
-    if (
-        nombre ==="" ||
-        edad ==="" ||
-        sexo ==="" ||
-        peso ==="" ||
-        tallaCintura ==="" ||
-        altura ==="" ||
-        grasaCorporal ==="" 
-    )
-    {
-        mensajeResultado.textContent = "Debe completar todos los campos";
-        mensajeResultado.className = "alert alert-danger mt-4";
-        return;
-    }
-    mensajeResultado.className = "alert alert-success mt-4";
-
-    formularioDatosForm.reset();
-});
-formularioAlimentacionForm.addEventListener("submit",function(evento){
-    evento.preventDefault();
-    const desayuno = campoDesayuno.value.trim();
-    const colacion1 = campoColacion.value.trim();
-    const almuerzo = campoAlmuerzo.value.trim();
-    const colacion2 = campoColacionTarde.value.trim();
-    const cena = campoCena.value.trim();
-    const observaciones = campoObservaciones.value.trim();
-    if (
-        desayuno ==="" ||
-        colacion1 ==="" ||
-        almuerzo ==="" ||
-        colacion2 ==="" ||
-        cena ==="" ||
-        observaciones ===""
-    )
-    {
-        mensajeResultado.textContent = "Debe completar todos los campos";
-        mensajeResultado.className = "alert alert-danger mt-4";
-        return;
-    }
-    mensajeResultado.className = "alert alert-success mt-4";
-
-    formularioAlimentacionForm.reset();
-});
-//Boton formulario de paciente en vista nutricionista
-
